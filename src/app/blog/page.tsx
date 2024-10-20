4a4eeece6e14edb3f4cffd96ff4217fe0b79ae56
Sumit Saurav <impossibleshadowstorm@gmail.com>
2024-10-20 19:06:06 +0530

    build v 1.0.0

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,15 +2,14 @@ import { Container } from "@/components/Container";
 import { Heading } from "@/components/Heading";
 import { Highlight } from "@/components/Highlight";
 import { Paragraph } from "@/components/Paragraph";
-import { Products } from "@/components/Products";
 import { getAllBlogs } from "../../../lib/getAllBlogs";
 import { Blogs } from "@/components/Blogs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Blogs | John Doe",
+  title: "Blogs | Sumit Saurav",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Sumit Saurav is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
 };
 
 export default async function Blog() {
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import localFont from "next/font/local";
 import { twMerge } from "tailwind-merge";
 
-// Font files can be colocated inside of `app`
+// Font files can be co-located inside of `app`
 const CalSans = localFont({
   src: [{ path: "../../fonts/CalSans-SemiBold.woff2" }],
   display: "swap",
@@ -15,7 +15,7 @@ export const Heading = ({
   as: Tag = "h1",
 }: {
   className?: string;
-  children: React.ReactNode;
+  children: any;
   as?: keyof JSX.IntrinsicElements;
 }) => {
   return (
